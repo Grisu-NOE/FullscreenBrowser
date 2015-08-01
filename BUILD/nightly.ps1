e2a402458d0e68665263ff4c9d554609f214c549
gregor4005 <gregor4005@gmail.com>
2015-08-01 08:32:54 +0200

    removed autosized

--- BUILD/nightly.ps1
+++ BUILD/nightly.ps1
@@ -1,3 +1,3 @@
 #ls | cp -Recurse -Destination $BUILD_STAGINGDIRECTORY\drop -Force
-Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* |  Select-Object DisplayName, DisplayVersion, Publisher, InstallDate | Format-Table –AutoSize
-Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object DisplayName, DisplayVersion, Publisher, InstallDate | Format-Table –AutoSized
+Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* |  Select-Object DisplayName, DisplayVersion, Publisher, InstallDate | Format-Table
+Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object DisplayName, DisplayVersion, Publisher, InstallDate | Format-Table
