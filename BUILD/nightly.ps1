ac474cd882824920face673786a49ff6b96022bf
gregor4005 <gregor4005@gmail.com>
2015-08-02 09:15:11 +0200

    Testing PS WebRequest

--- BUILD/nightly.ps1
+++ BUILD/nightly.ps1
@@ -1,3 +1,5 @@
-#ls | cp -Recurse -Destination $BUILD_STAGINGDIRECTORY\drop -Force
-Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* |  Select-Object DisplayName, DisplayVersion, Publisher, InstallDate | Format-Table
-Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object DisplayName, DisplayVersion, Publisher, InstallDate | Format-Table
+# List installed software
+#Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* |  Select-Object DisplayName, DisplayVersion, Publisher, InstallDate | Format-Table
+#Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object DisplayName, DisplayVersion, Publisher, InstallDate | Format-Table
+Invoke-WebRequest -?
+$PSVersionTable.PSVersion
