e199a1440199b47ea150b385d843ac35e96f2400
gregor4005 <gregor4005@gmail.com>
2015-08-02 22:01:46 +0200

    Updated incrementVersion.ps1

--- BUILD/incrementVersion.ps1
+++ BUILD/incrementVersion.ps1
@@ -1,13 +1,4 @@
-if($BUILD_SOURCESDIRECTORY)
-{
-	$file = $BUILD_SOURCESDIRECTORY
-}
-else
-{
-	$file = "C:\Git\grisu-noe-infoscreen"
-}
-
-$file = $file + "\Version.txt"
+$file = $BUILD_SOURCESDIRECTORY + "\Version.txt"
 $fileVersion = [version](Get-Content $file | Select -First 1)
 $newVersion = "{0}.{1}.{2}.{3}" -f $fileVersion.Major, $fileVersion.Minor, $fileVersion.Build, ($fileVersion.Revision + 1)
 $newVersion | Set-Content $file
