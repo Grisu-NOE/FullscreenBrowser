0735a5c564aaaac653b7afaa998f0ff9ab8901f0
gregor4005 <gregor4005@gmail.com>
2015-08-02 23:23:35 +0200

    Updated incrementVersion.ps1

--- BUILD/incrementVersion.ps1
+++ BUILD/incrementVersion.ps1
@@ -1,16 +1,4 @@
-# Make sure path to source code directory is available
-if (-not $Env:BUILD_SOURCESDIRECTORY)
-{
-    Write-Error ("BUILD_SOURCESDIRECTORY environment variable is missing.")
-    exit 1
-}
-elseif (-not (Test-Path $Env:BUILD_SOURCESDIRECTORY))
-{
-    Write-Error "BUILD_SOURCESDIRECTORY does not exist: $Env:BUILD_SOURCESDIRECTORY"
-    exit 1
-}
 Write-Verbose "BUILD_SOURCESDIRECTORY: $Env:BUILD_SOURCESDIRECTORY"
-
 $file = $Env:BUILD_SOURCESDIRECTORY + "\Version.txt"
 $fileVersion = [version](Get-Content $file | Select -First 1)
 $newVersion = "{0}.{1}.{2}.{3}" -f $fileVersion.Major, $fileVersion.Minor, $fileVersion.Build, ($fileVersion.Revision + 1)
