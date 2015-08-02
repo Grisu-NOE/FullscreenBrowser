179d1acbebb219d914a2b066f1030b027b85c0f0
gregor4005 <gregor4005@gmail.com>
2015-08-03 00:16:27 +0200

    Improved output

--- BUILD/incrementVersion.ps1
+++ BUILD/incrementVersion.ps1
@@ -2,5 +2,7 @@ Write-Host "BUILD_SOURCESDIRECTORY: $Env:BUILD_SOURCESDIRECTORY"
 $file = $Env:BUILD_SOURCESDIRECTORY + "\Version.txt"
 $fileVersion = [version](Get-Content $file | Select -First 1)
 $newVersion = "{0}.{1}.{2}.{3}" -f $fileVersion.Major, $fileVersion.Minor, $fileVersion.Build, ($fileVersion.Revision + 1)
-Write-Host "Version: $newVersion"
+Write-Host "Calculated version: $newVersion"
 $newVersion | Set-Content $file
+$newFileVersion = [version](Get-Content $file | Select -First 1)
+Write-Host "Stored version: $newFileVersion"
