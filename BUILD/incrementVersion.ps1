fa64b72400fd8ce67200fdf4c80bc38004fd2abc
gregor4005 <gregor4005@gmail.com>
2015-08-02 23:53:40 +0200

    Change output pipe

--- BUILD/incrementVersion.ps1
+++ BUILD/incrementVersion.ps1
@@ -1,6 +1,6 @@
-Write-Verbose "BUILD_SOURCESDIRECTORY: $Env:BUILD_SOURCESDIRECTORY"
+Write-Host "BUILD_SOURCESDIRECTORY: $Env:BUILD_SOURCESDIRECTORY"
 $file = $Env:BUILD_SOURCESDIRECTORY + "\Version.txt"
 $fileVersion = [version](Get-Content $file | Select -First 1)
 $newVersion = "{0}.{1}.{2}.{3}" -f $fileVersion.Major, $fileVersion.Minor, $fileVersion.Build, ($fileVersion.Revision + 1)
-Write-Verbose "Version: $newVersion"
+Write-Host "Version: $newVersion"
 $newVersion | Set-Content $file
