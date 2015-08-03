ab3da98618bacaab00e7c25296d52358bec0c752
gregor4005 <gregor4005@gmail.com>
2015-08-03 11:46:28 +0200

    Dummy commit to test CI

--- BUILD/nightly.ps1
+++ BUILD/nightly.ps1
@@ -1,6 +1,5 @@
 # List installed software
 #Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* |  Select-Object DisplayName, DisplayVersion, Publisher, InstallDate | Format-Table
 #Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object DisplayName, DisplayVersion, Publisher, InstallDate | Format-Table
-Invoke-WebRequest -?
-$PSVersionTable.PSVersion
+
 Write-Host Compression level is $CompressionLevel
