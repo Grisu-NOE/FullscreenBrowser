74485a6b15d9572eaf06a56221555f28c89ddda0
gregor4005 <gregor4005@gmail.com>
2015-08-03 16:12:09 +0200

    Updated scripts

--- BUILD/incrementVersion.ps1
+++ BUILD/incrementVersion.ps1
@@ -2,10 +2,8 @@ Write-Host "BUILD_SOURCESDIRECTORY: $Env:BUILD_SOURCESDIRECTORY"
 $file = $Env:BUILD_SOURCESDIRECTORY + "\Version.txt"
 $fileVersion = [version](Get-Content $file | Select -First 1)
 $newVersion = "{0}.{1}.{2}.{3}" -f $fileVersion.Major, $fileVersion.Minor, $fileVersion.Build, ($fileVersion.Revision + 1)
-Write-Host "Calculated version: $newVersion"
-#$newVersion | Set-Content $file
-#$newFileVersion = [version](Get-Content $file | Select -First 1)
-#Write-Host "Stored version: $newFileVersion"
+Write-Host "New version: $newVersion"
+$newVersion | Set-Content $file
 
 $getAnswer = Invoke-WebRequest -UseBasicParsing -Uri https://api.github.com/repos/Grisu-NOE/Infoscreen/contents/Version.txt -Method Get
 if (-not $getAnswer.StatusCode.Equals(200))
