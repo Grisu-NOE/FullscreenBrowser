2d722d0b65cd5ab56ebf6b4dab2810f600ca7fb1
gregor4005 <gregor4005@gmail.com>
2015-08-03 15:06:13 +0200

    Added build number to nightly.ps1

--- BUILD/nightly.ps1
+++ BUILD/nightly.ps1
@@ -2,4 +2,8 @@
 #Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* |  Select-Object DisplayName, DisplayVersion, Publisher, InstallDate | Format-Table
 #Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object DisplayName, DisplayVersion, Publisher, InstallDate | Format-Table
 
-Write-Host Compression level is $CompressionLevel
+Write-Host Compression level is $Env:CompressionLevel
+$file = $Env:BUILD_SOURCESDIRECTORY + "\Version.txt"
+$fileVersion = [version](Get-Content $file | Select -First 1)
+$Env:BUILD_BUILDNUMBER = "NIGHTLY-$fileVersion"
+Write-Host "Build number is $Env:BUILD_BUILDNUMBER"
