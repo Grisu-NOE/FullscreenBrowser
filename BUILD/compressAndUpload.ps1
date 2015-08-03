1e599088ca4c643504da97b31993b354a26e5637
gregor4005 <gregor4005@gmail.com>
2015-08-03 18:17:47 +0200

    Archive is now sfx

--- BUILD/compressAndUpload.ps1
+++ BUILD/compressAndUpload.ps1
@@ -6,26 +6,9 @@ $file = $Env:BUILD_SOURCESDIRECTORY + "\Version.txt"
 $fileVersion = [version](Get-Content $file | Select -First 1)
 Write-Host "Version is $fileVersion"
 
-#$folder = $Env:AGENT_BUILDDIRECTORY + "\drop"
-#New-Item -ItemType Directory -Force -Path $folder
-
 $binaries = "$Env:BUILD_STAGINGDIRECTORY\$fileVersion"
-
 Move-Item "$Env:BUILD_STAGINGDIRECTORY\*" $binaries
 
-Write-Host "Content in $binaries"
-ls $binaries -Recurse
-
-#Write-Host """$Env:BUILD_SOURCESDIRECTORY\BUILD\7z\7z.exe"" a -t7z -mx$Env:CompressionLevel -mmt -ms -sfx ""$Env:BUILD_STAGINGDIRECTORY\$fileVersion.exe"" ""$binaries"""
-#& "$Env:BUILD_SOURCESDIRECTORY\BUILD\7z\7z.exe" a -t7z -mx$Env:CompressionLevel -mmt -ms -sfx "$Env:BUILD_STAGINGDIRECTORY\$fileVersion.exe" "$binaries"
-
-Write-Host """$Env:BUILD_SOURCESDIRECTORY\BUILD\7z\7z.exe"" a -t7z -m0=lzma -mx=$Env:CompressionLevel -mfb=64 -md=128m -ms=on -sfx ""$Env:BUILD_STAGINGDIRECTORY\$fileVersion.exe"" ""$binaries"""
-& "$Env:BUILD_SOURCESDIRECTORY\BUILD\7z\7z.exe" a -t7z -m0=lzma -mx=$Env:CompressionLevel -mfb=64 -md=128m -ms=on -sfx "$Env:BUILD_STAGINGDIRECTORY\$fileVersion.exe" "$binaries"
-
-Write-Host "Try Start-Process"
 $process = "$Env:BUILD_SOURCESDIRECTORY\BUILD\7z\7z.exe"
-Start-Process -FilePath $process -ArgumentList "a -t7z -m0=lzma2 -mx=$Env:CompressionLevel -mfb=64 -md=128m -ms=on ""$Env:BUILD_STAGINGDIRECTORY\$fileVersion.7z"" ""$binaries""" -Wait -NoNewWindow -PassThru
+Start-Process -FilePath $process -ArgumentList "a -t7z -m0=lzma2 -mx=$Env:CompressionLevel -mfb=64 -md=128m -ms=on -sfx ""$Env:BUILD_STAGINGDIRECTORY\$fileVersion.exe"" ""$binaries""" -Wait -NoNewWindow -PassThru
 
-ls $Env:BUILD_STAGINGDIRECTORY
-
-& "$Env:BUILD_SOURCESDIRECTORY\BUILD\7z\7z.exe" -?
