a7e1b02e517f8b0150c27a16d210263ed365a8c6
gregor4005 <gregor4005@gmail.com>
2015-08-03 18:11:12 +0200

    Try 7z with Start-Process

--- BUILD/compressAndUpload.ps1
+++ BUILD/compressAndUpload.ps1
@@ -22,8 +22,9 @@ ls $binaries -Recurse
 Write-Host """$Env:BUILD_SOURCESDIRECTORY\BUILD\7z\7z.exe"" a -t7z -m0=lzma -mx=$Env:CompressionLevel -mfb=64 -md=128m -ms=on -sfx ""$Env:BUILD_STAGINGDIRECTORY\$fileVersion.exe"" ""$binaries"""
 & "$Env:BUILD_SOURCESDIRECTORY\BUILD\7z\7z.exe" a -t7z -m0=lzma -mx=$Env:CompressionLevel -mfb=64 -md=128m -ms=on -sfx "$Env:BUILD_STAGINGDIRECTORY\$fileVersion.exe" "$binaries"
 
-Write-Host """$Env:BUILD_SOURCESDIRECTORY\BUILD\7z\7z.exe"" a -t7z -m0=lzma -mx=$Env:CompressionLevel -mfb=64 -md=128m -ms=on -sfx ""$Env:BUILD_STAGINGDIRECTORY\$fileVersion.exe"" ""$binaries"""
-& "$Env:BUILD_SOURCESDIRECTORY\BUILD\7z\7z.exe" a -t7z -m0=lzma -mx=$Env:CompressionLevel -mfb=64 -md=128m -ms=on "$Env:BUILD_STAGINGDIRECTORY\$fileVersion.7z" "$binaries"
+Write-Host "Try Start-Process"
+$process = "$Env:BUILD_SOURCESDIRECTORY\BUILD\7z\7z.exe"
+Start-Process -FilePath $process -ArgumentList "a -t7z -m0=lzma2 -mx=$Env:CompressionLevel -mfb=64 -md=128m -ms=on ""$Env:BUILD_STAGINGDIRECTORY\$fileVersion.7z"" ""$binaries""" -Wait -NoNewWindow -PassThru
 
 ls $Env:BUILD_STAGINGDIRECTORY
 
