9c655d006df3d41fb59987111ac40038bd24ee8f
gregor4005 <gregor4005@gmail.com>
2015-08-03 17:14:28 +0200

    Testing different 7z arguments

--- BUILD/compressAndUpload.ps1
+++ BUILD/compressAndUpload.ps1
@@ -13,6 +13,15 @@ $binaries = "$Env:BUILD_STAGINGDIRECTORY\$fileVersion"
 
 Move-Item "$Env:BUILD_STAGINGDIRECTORY\*" $binaries
 
+ls $binaries
+
+
 Write-Host "Compression level is $Env:CompressionLevel"
-Write-Host """$Env:BUILD_SOURCESDIRECTORY\BUILD\7z\7z.exe"" a -t7z -mx$Env:CompressionLevel -mmt -ms -sfx ""$fileVersion.exe"" $binaries -xr!logs"
-& "$Env:BUILD_SOURCESDIRECTORY\BUILD\7z\7z.exe" a -t7z -mx$Env:CompressionLevel -mmt -ms -sfx "$fileVersion.exe" $binaries -xr!logs
+Write-Host """$Env:BUILD_SOURCESDIRECTORY\BUILD\7z\7z.exe"" a -t7z -mx$Env:CompressionLevel -mmt -ms -sfx $fileVersion.exe ""$binaries"" -xr!logs"
+& "$Env:BUILD_SOURCESDIRECTORY\BUILD\7z\7z.exe" a -t7z -mx$Env:CompressionLevel -mmt -ms -sfx $fileVersion.exe "$binaries" -xr!logs
+
+Write-Host """$Env:BUILD_SOURCESDIRECTORY\BUILD\7z\7z.exe"" a -t7z -mx$Env:CompressionLevel -mmt -ms -sfx abc.exe ""$binaries\"" -xr!logs"
+& "$Env:BUILD_SOURCESDIRECTORY\BUILD\7z\7z.exe" a -t7z -mx$Env:CompressionLevel -mmt -ms -sfx abc.exe "$binaries\" -xr!logs
+
+Write-Host """$Env:BUILD_SOURCESDIRECTORY\BUILD\7z\7z.exe"" a -t7z -mx$Env:CompressionLevel -mmt -ms -sfx def.exe ""$binaries\*"" -xr!logs"
+& "$Env:BUILD_SOURCESDIRECTORY\BUILD\7z\7z.exe" a -t7z -mx$Env:CompressionLevel -mmt -ms -sfx def.exe "$binaries\*" -xr!logs
