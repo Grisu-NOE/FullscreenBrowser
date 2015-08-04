57ac93c1a5f113aa0b7897c20e1206ce903c326b
gregor4005 <gregor4005@gmail.com>
2015-08-04 15:56:46 +0200

    Added creation of release

--- BUILD/compressAndUpload.ps1
+++ BUILD/compressAndUpload.ps1
@@ -10,5 +10,41 @@ $binaries = "$Env:BUILD_STAGINGDIRECTORY\$fileVersion"
 Move-Item "$Env:BUILD_STAGINGDIRECTORY\*" $binaries
 
 $process = "$Env:BUILD_SOURCESDIRECTORY\BUILD\7z\7z.exe"
-Start-Process -FilePath $process -ArgumentList "a -t7z -m0=lzma2 -mx=$Env:CompressionLevel -mfb=64 -md=128m -ms=on -sfx ""$Env:BUILD_STAGINGDIRECTORY\$fileVersion.exe"" ""$binaries""" -Wait -NoNewWindow -PassThru
+$compressedFileName = "$fileVersion.exe"
+$compressedFilePath = "$Env:BUILD_STAGINGDIRECTORY\$compressedFileName"
+Start-Process -FilePath $process -ArgumentList "a -t7z -m0=lzma2 -mx=$Env:CompressionLevel -mfb=64 -md=128m -ms=on -sfx ""$compressedFilePath"" ""$binaries""" -Wait -NoNewWindow -PassThru
 
+$body = @{
+  "tag_name"="v$fileVersion"
+  "target_commitish"="master"
+  "name"="v$fileVersion"
+  "body"="#NIGHTLY build`n##Visual Studio`n* Name of the Build Definition: $Env:BUILD_DEFINITIONNAME`n* Build number: $Env:BUILD_BUILDNUMBER`n * Build URI: $Env:BUILD_BUILDURI"
+  "draft"=$Env:GitDraft
+  "prerelease"=$Env:GitPreRelease
+}
+
+$auth = @{
+  "Authorization"="token " + $Env:GitToken
+}
+
+$releaseAnswer = Invoke-WebRequest -Headers $auth -UseBasicParsing -Uri "https://api.github.com/repos/Grisu-NOE/Infoscreen/releases" -Method Post -ContentType "application/json; charset=utf-8" -Body $($body | ConvertTo-Json -Depth 5 -Compress)
+Write-Host "Release status code is $($releaseAnswer.StatusCode) $($releaseAnswer.StatusDescription)"
+
+$releaseContent = $releaseAnswer.Content | ConvertFrom-Json
+$uri = $releaseContent.upload_url.Replace("{?name}","?name=$compressedFileName")
+
+switch([System.IO.Path]::GetExtension($compressedFileName))
+{
+  ".7z" { $contentType = "application/x-7z-compressed" }
+  ".exe" { $contentType = "application/x-msdownload" }
+  ".msi" { $contentType = "application/x-msi" }
+  ".rar" { $contentType = "application/x-rar-compressed" }
+  ".zip" { $contentType = "application/zip" }
+  default {
+    Write-Error "Extension not supported by script"
+    exit 1
+  }
+}
+
+$uploadAnswer = Invoke-WebRequest -Headers $auth -UseBasicParsing -Uri "$uri" -Method Post -ContentType "$contentType" -InFile "$compressedFilePath"
+Write-Host "Upload status code is $($uploadAnswer.StatusCode) $($uploadAnswer.StatusDescription)"
