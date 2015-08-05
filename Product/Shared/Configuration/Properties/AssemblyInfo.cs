c4bbfd45224ab603912483ff95555736e829988b
gregor4005 <gregor4005@gmail.com>
2015-08-05 23:50:24 +0200

    Fixed StyleCop warnings

--- Product/Shared/Configuration/Properties/AssemblyInfo.cs
+++ Product/Shared/Configuration/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@
 //       Austraße 33
 //       A-3500 Krems/Donau
 //       Austria
-// 
+//
 //       Tel.:   +43 (0)2732 85522
 //       Fax.:   +43 (0)2732 85522 40
 //       E-mail: office@feuerwehr-krems.at
-// 
+//
 //       This software is furnished under a license and may be
 //       used  and copied only in accordance with the terms of
 //       such  license  and  with  the  inclusion of the above
@@ -16,25 +16,25 @@
 //       thereof   may  not  be  provided  or  otherwise  made
 //       available  to  any  other  person.  No  title  to and
 //       ownership of the software is hereby transferred.
-// 
+//
 //       The information in this software is subject to change
 //       without  notice  and  should  not  be  construed as a
 //       commitment by Freiwillige Feuerwehr Krems/Donau.
-// 
+//
 //  </copyright>
 //  --------------------------------------------------------------------------------------------------------------------
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Configuration")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
--- Product/Shared/Configuration/XML/BrowserConfig.cs
+++ Product/Shared/Configuration/XML/BrowserConfig.cs
@@ -4,11 +4,11 @@
 //       Austraße 33
 //       A-3500 Krems/Donau
 //       Austria
-// 
+//
 //       Tel.:   +43 (0)2732 85522
 //       Fax.:   +43 (0)2732 85522 40
 //       E-mail: office@feuerwehr-krems.at
-// 
+//
 //       This software is furnished under a license and may be
 //       used  and copied only in accordance with the terms of
 //       such  license  and  with  the  inclusion of the above
@@ -16,11 +16,11 @@
 //       thereof   may  not  be  provided  or  otherwise  made
 //       available  to  any  other  person.  No  title  to and
 //       ownership of the software is hereby transferred.
-// 
+//
 //       The information in this software is subject to change
 //       without  notice  and  should  not  be  construed as a
 //       commitment by Freiwillige Feuerwehr Krems/Donau.
-// 
+//
 //  </copyright>
 //  --------------------------------------------------------------------------------------------------------------------
 namespace At.FF.Krems.Configuration.XML
